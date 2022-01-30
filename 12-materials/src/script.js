53208c741183e5fa0fa106def6a7a9d0a0292ada
Charlie Fischer <charlie.a.fischer@gmail.com>
2022-01-30 16:14:29 +0000

    aaa

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -15,8 +15,13 @@ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
-const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/4.png')
+const gradientTexture = textureLoader.load('/textures/gradients/5.jpg')
+gradientTexture.minFilter = THREE.NearestFilter
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+
 
 /**
  * Base
@@ -30,15 +35,36 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const material = new THREE.MeshBasicMaterial()
-material.map = doorColourTexture
-// material.color = new THREE.Color('purple') // have to specify THREE.Color to set it as rgb values
-// material.wireframe = true
-// material.opacity = 0.5
-material.transparent = true
-material.alphaMap = doorAlphaTexture
-material.side = THREE.FrontSide // set which side of the face is visible
+// const material = new THREE.MeshBasicMaterial()
+// material.map = doorColourTexture
+// // material.color = new THREE.Color('purple') // have to specify THREE.Color to set it as rgb values
+// // material.wireframe = true
+// // material.opacity = 0.5
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.FrontSide // set which side of the face is visible
 
+
+// const material = new THREE.MeshNormalMaterial()
+// material.flatShading = true // no longer smooth - shows a squared off curves
+
+// const material = new THREE.MeshMatcapMaterial() // display colour by using the normals as a reference to pick the right colour on a texture that looks like a sphere
+// material.matcap = matcapTexture // can simulate light without having lights in the scene
+
+// const material = new THREE.MeshDepthMaterial() // white if close dark if far away
+
+// const material = new THREE.MeshLambertMaterial() // reacts to light but can have poor rendering quality
+
+// const material = new THREE.MeshPhongMaterial() // like Lambert but no issues & light can bounce 
+// material.shininess = 100
+// material.specular = new THREE.Color(0xff1180) // coloured spot
+
+// const material = new THREE.MeshToonMaterial() // cartoony
+// material.gradientMap = gradientTexture
+
+const material = new THREE.MeshStandardMaterial() // best - supports light but more realistic and has roughness and metalness params
+material.metalness = 0.9
+material.roughness = 0.3
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
@@ -60,6 +86,16 @@ torus.position.x = 1.5
 
 scene.add(sphere, plane, torus)
 
+/** 
+ * Lights
+ * */  
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+ scene.add(ambientLight)
+ const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+ scene.add(pointLight)
 
 /**
  * Sizes
